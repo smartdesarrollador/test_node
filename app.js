43eaeb0fb34224d91f7085bd2b2a18867001d95c
Jeans <sistemadesignstyle@gmail.com>
2024-08-04 17:17:16 -0500

    iniciando node

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/", function (req, res) {
+  res.send("<h1><center><ul>¡Hello World from node js! </ul></center></h1>");
+});
+
+app.listen(3000, function (req, res) {
+  console.log("Server listening on port 3000");
+});
