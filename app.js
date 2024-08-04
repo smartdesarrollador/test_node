a3d6c7868a48cf71d8e3b2e5deb7762c85cb4665
Jeans <sistemadesignstyle@gmail.com>
2024-08-04 17:34:27 -0500

    agregando palabra

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.send("<h1><center><ul>¡Hello World from node js! </ul></center></h1>");
+  res.send(
+    "<h1><center><ul>¡Hello World from node js hola! </ul></center></h1>"
+  );
 });
 
 app.listen(3000, function (req, res) {
